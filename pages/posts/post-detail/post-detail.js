b1fcfc7f2755acd6512cd0e0d745c0cd65953131
黄文鉴 <631236933@qq.com>
2018-05-29 18:13:50 +0800

    文章收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,18 +6,102 @@ Page({
         var postId = option.id;         //id是在navigateTo的url里拼进来的参数
         var postData = postsData.postList[postId];
         this.setData({
-            postData: postData
+            postData: postData,
+            currentPostId: postId
         })
         
-       
-        
+        var postsCollected = wx.getStorageSync('posts_collected');
+        if (postsCollected) {
+            var postCollected = postsCollected[postId];
+            this.setData({
+                collected: postCollected
+            })
+        } else {
+            var postsCollected = {};
+            postsCollected[postId] = false;
+            wx.setStorageSync('posts_collected', postsCollected);
+        }
     },
     
     onCollectionTap: function (event) {
+        this.getPostsCollectedSync();
+        //this.getPostsCollectedAsync();
+    },
+    
+    getPostsCollectedAsync: function () {
+        var that = this;
+        wx.getStorage({
+            key: 'posts_collected',
+            success: res => {
+                var postsCollected = res.data;
+                var postCollected = postsCollected[that.data.currentPostId];
+                postCollected = !postCollected;
+                postsCollected[that.data.currentPostId] = postCollected;
+                
+                //wx.showToast({
+                //    title: postCollected ? "收藏成功" : "取消成功"
+                //})
+                that.showModal(postsCollected, postCollected);
+            }
+        });
+    },
+    
+    getPostsCollectedSync: function () {
+        var postsCollected = wx.getStorageSync('posts_collected');
+        var postCollected = postsCollected[this.data.currentPostId];
+        postCollected = !postCollected;
+        postsCollected[this.data.currentPostId] = postCollected;
+        
+        //wx.showToast({
+        //    title: postCollected ? "收藏成功" : "取消成功"
+        //})
+        this.showModal(postsCollected, postCollected);
+    },
+    
+    showModal: function (postsCollected, postCollected) {
+        var that = this;
+        wx.showModal({
+            title: '收藏',
+            content: postCollected ? '收藏该文章?' : '取消收藏该文章',
+            confirmText: "确认",
+            confirmColor: "#405f80",
+            showCancel: true,
+            cancelText: "取消",
+            cancelColor: "#333",
+            success: function (res) {
+                if (res.confirm) {
+                    //更新文章的缓存值
+                    wx.setStorageSync('posts_collected', postsCollected);
+                    //更新数据绑定的变量,从而实现图片切换
+                    that.setData({
+                        collected: postCollected
+                    })
+                }
+            }
+        })
+    },
+    
+    showToast: function (postsCollected, postCollected) {
+    
     },
     
     onShareTap: function (event) {
-        //缓存的上限最大不能超过10MB
+        var itemList = [
+            "分享给微信好友",
+            "分享到朋友圈",
+            "分享到QQ",
+            "分享到微博"
+        ]
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: "#405f80",
+            success: res => {
+                wx.showModal({
+                    title: "用户: " + itemList[tapIndex],
+                    content: "现在无法实现分享功能"
+                })
+            }
+        })
     }
     
 })
